fe315d0d67cb8ecfebccd38a443a651e4b11c2f5
Jonathan R. Madsen <jrmadsen@users.noreply.github.com>
2022-04-27 14:31:20 -0500

    VERSION + git submodules
    - ensure submodules are cloned when bundling the sdist

--- scripts/bundle-sdist.sh
+++ scripts/bundle-sdist.sh
@@ -8,6 +8,14 @@ fi
 
 : ${PYTHON_EXECUTABLE:=python3}
 
+if [ ! -f external/kokkos/CMakeLists.txt ]; then
+    git submodule update --init external/kokkos
+fi
+
+if [ ! -f external/pybind11/CMakeLists.txt ]; then
+    git submodule update --init external/pybind11
+fi
+
 rm -rf .eggs *.egg-info _skbuild dist
 ${PYTHON_EXECUTABLE} setup.py sdist -v -v -v
 cd dist
